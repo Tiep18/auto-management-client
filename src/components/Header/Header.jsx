9437183c28b5eb017bd7c33fd4dc422d2c42062e
Pham Tuan Anh <anh.phamtuan@Phams-MacBook-Air.local>
2023-05-13 22:18:06 +0700

    add logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Header as AntdHeader } from 'antd/es/layout/layout'
 import { Breadcrumb } from 'antd'
 import { Link, useLocation } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { faRightToBracket, faSignOut } from '@fortawesome/free-solid-svg-icons'
 import { menuConfig } from '../../config/config'
 import { useSelector } from 'react-redux'
 
@@ -25,6 +25,12 @@ const Header = () => {
 
     return res
   }, [pathname])
+
+  const handleLogout = () => {
+    localStorage.removeItem('_at')
+    localStorage.removeItem('_rt')
+    window.location.reload()
+  }
   return (
     <AntdHeader className="bgc-fa h-[78px] p-4 my-[10px] mx-5 flex items-center">
       <div className="flex flex-col justify-between h-full">
@@ -33,20 +39,16 @@ const Header = () => {
           {items[items.length - 1]?.title}
         </h2>
       </div>
-
-      {currentUser ? ( // check if login
-        <h3 className="leading-none ml-auto text-gray-500 text-[16px] font-semibold flex gap-2 items-center">
-          Hello, {currentUser.fullName}
-        </h3>
-      ) : (
-        <Link
-          className="leading-none ml-auto text-gray-500 text-[16px] font-semibold flex gap-2 items-center"
-          to="/login"
+      <h3 className="leading-none ml-auto text-[18px] font-semibold flex gap-2 items-center">
+        Hello, {currentUser.fullName}
+        <span
+          className=" ml-4 leading-none  text-gray-500 text-[14px] font-semibold flex gap-2 items-center cursor-pointer hover:opacity-70"
+          onClick={handleLogout}
         >
-          <FontAwesomeIcon icon={faRightToBracket} />
-          Sign in
-        </Link>
-      )}
+          Sign out
+          <FontAwesomeIcon icon={faSignOut} />
+        </span>
+      </h3>
     </AntdHeader>
   )
 }
